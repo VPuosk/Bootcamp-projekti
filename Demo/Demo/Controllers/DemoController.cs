3c1b1708b915c24121914e363ce617dd5fd6591a
VPuosk <vesa.puoskari@gmail.com>
2021-04-07 17:00:48 +0300

    Testausta Model/View/Controller järjestelmän kanssa ja tiedon välittämistä yhdestä tilasta toiseen.

---
+++ Demo/Demo/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["laskuri"] = 20;
+            return View();
+        }
+
+        //GET
+        public IActionResult Laskuri()
+        {
+            //ViewData["laskuri"] = 20;
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Laskuri(Models.Arvopari arvopari)
+        {
+            Console.WriteLine(arvopari.A);
+            Console.WriteLine(arvopari.B);
+            ViewData["laskuri"] = arvopari.Summa();
+            return View();
+        }
+    }
+}
---
+++ Demo/Demo/Models/Arvopari.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class Arvopari
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public int Summa()
+        {
+            return A + B;
+        }
+
+        public int Erotus()
+        {
+            return A - B;
+        }
+
+        public int Kertolasku()
+        {
+            return A * B;
+        }
+
+        public int Jakolasku()
+        {
+            return A / B;
+        }
+        
+        public int Modulo()
+        {
+            return A % B;
+        }
+    }
+}
