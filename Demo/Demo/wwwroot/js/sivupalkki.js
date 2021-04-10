4e1d3470b4e3ae4d65e31acd34aa007c230d92d3
VPuosk <vesa.puoskari@gmail.com>
2021-04-10 14:08:57 +0300

    Lisätty sivupalkki valikkoon ja muokattu navigaation toimintaa. Lisätty CV:n HY:n opintotietoja ja omia yliopisto-opintoja (mm. linkki graduun).

---
+++ Demo/Demo/wwwroot/js/sivupalkki.js
@@ -0,0 +1,13 @@
+﻿// TARKOITETTU SIVUNAVIGOINNIN TOIMINTAAN
+// lähde: W3School
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function avaaSivupalkki() {
+    document.getElementById("sivuPalkki").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function suljeSivupalkki() {
+    document.getElementById("sivuPalkki").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
