524c7bc1eb631d31e06c2f79f83f03c5d9f5bda8
VPuosk <vesa.puoskari@gmail.com>
2021-04-18 20:23:12 +0300

    Aloitettu tietokanta osuuden lisääminen - tosin tämän rakenne tullee muuttumaan.

---
+++ Demo/Demo/Controllers/TietokantaController.cs
@@ -0,0 +1,20 @@
+﻿using Demo.ReitinhaunLuokat;
+using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    public class TietokantaController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
