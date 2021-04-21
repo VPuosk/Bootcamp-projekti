99f08bf24dcb253f9d260580b59b57b2f69e1fb6
VPuosk <vesa.puoskari@gmail.com>
2021-04-21 15:23:58 +0300

    Lisätty luontokuva osuus (linkitys dropboxiin)

--- Demo/Demo/Controllers/HomeController.cs
+++ Demo/Demo/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace Demo.Controllers
             return View();
         }
 
+        public IActionResult Luonto()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
